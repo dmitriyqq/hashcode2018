8a1abcd5b2da269e8d0e213a88868bf6bfa74da2
dmitriyqq <shulaykin1@gmail.com>
2018-03-06 14:41:54 +0300

    29m

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 
 using std::cout;
 using std::endl;
-
+using std::vector;
 
 struct Ride{
     int x1, x2;
     int y1, y2;
     int s, f;
+    int id;
 
-    Ride(){
+    static int counter;
 
-    }
+    Ride() = default;
 
     Ride(int x1, int y1, int x2, int y2, int s, int f):
             x1(x1), x2(x2), y1(y1), y2(y2), s(s), f(f){
@@ -19,11 +20,12 @@ struct Ride{
     }
 
     bool operator<(const Ride & o){
-        return this->s < o.s;
+        return this->f < o.f;
     }
 
     friend std::istream& operator>>(std::istream &is, Ride & r){
         is>>r.x1>>r.y1>>r.x2>>r.y2>>r.s>>r.f;
+        r.id = counter++;
         return is;
     }
 
@@ -34,24 +36,23 @@ struct Ride{
     }
 };
 
+int Ride::counter = 0;
+
 
 struct Car{
     int x = 0, y = 0;
     int avail = 0;
     int id = -1;
 
-    Car(){
-        id = -1;
-    }
+    Car() = default;
 
-    Car(int id, int x = 0, int y = 0 ) :
+    explicit Car(int id, int x = 0, int y = 0):
             x(x), y(y), id(id){
 
     }
 
     Car& operator = (const Car & c) = default;
-
-
+    
     friend std::ostream& operator<<(std::ostream &os, Car & r){
         os<<"id: "<<r.id<<" x="<<r.x<<" y="<<r.y<<" avail="<<r.avail;
         return os;
@@ -63,106 +64,46 @@ struct Car{
         avail = na;
         cout<<"updateted: "<<*this<<endl;
     }
-
-
+    
 };
 
-using std::vector;
+
 
 void solve(const std::string & input, const std::string & output){
     std::ifstream in(input);
     std::ofstream out(output);
     int r, c, n, f, b, t;
+    
     in>>r; // number of rows of the grid
     in>>c; // number of columns of the grid
     in>>n; // number of vehicles in the fleet
     in>>f; // number of rides
     in>>b; // per-ride bonus for starting the ride on time
     in>>t; // number of steps in the simulation
-    std::vector <Ride> v(f, Ride());
+    
+    vector <Ride> rides(f, Ride());
+    
     for(int i = 0; i < f; i++){
-        in>>v[i];
+        in>>rides[i];
     }
-    std::set <int> free;
-    std::sort(v.begin(), v.end());
-    std::vector < std::vector <int> > vr (n, std::vector <int>());
-    vector<Car> avto;
+    
+    std::sort(rides.begin(), rides.end());
+    
+    vector < vector <int> > assigned_rides(n, vector <int>());
+    
+    vector<Car> cars;
 
     for(int i = 0; i < n; i++){
-        avto.emplace_back(i+1);
-        free.insert(i+1);
+        cars.emplace_back(i);
     }
 
-    for(int i =0 ; i< f; i++){
-        vector <Car> avaible;
-        int cs = v[i].s;
-        int cf = v[i].f;
-        cout<<"going to "<<v[i]<<endl;
-        using std::abs;
-
-        Ride& dest = v[i];
-        int toDest = abs(dest.x2 - dest.x1) + abs(dest.y2 - dest.y1);
-        for(auto &e : avto){
-
-            int toStart = abs(e.x - dest.x1) + abs(e.y - dest.y1);
-            int a = std::max(cs + toDest, e.avail +  toStart + toDest);
-
-            if(toStart + e.avail <= cf){
-               // cout<<"car good adding"<<endl;
-                avaible.push_back(e);
-                continue;
-            }else {
-              //  cout << "car:  " << e;
-              //  cout << "tostart:  " << toStart;
-              //  cout << "  arrive: " << a << endl;
-            }
-        }
-
-        Car best;
-        int nearest = INT_MAX;
-        cout<<"availble: "<<avaible.size()<<endl;
-        for(auto &e : avaible){
-            //cout<<e<<endl;
-            int toStart = abs(e.x - dest.x1) + abs(e.y - dest.y1)+e.avail;
-
-            if(nearest > toStart){
-                nearest = toStart;
-                best = e;
-            }
-        }
-
-        cout<<"nearest = "<<nearest<<endl;
-
-        if(best.id != -1){
-            cout<<"best car = "<<endl<<best<<endl;
-            vr[best.id-1].push_back(i);
-            int toStart = abs(best.x - dest.x1) + abs(best.y - dest.y1);
-            int a = best.avail +  toStart + toDest;
-            int id = best.id;
-            avto[id-1].update(dest.x2, dest.y2, a);
-
-
-        }else{
-            cout<<"Noto possible to service best -1" <<endl;
-            int minj = 0;
-            int mm = 0;
-            for(int i = 0; i < vr.size(); i++)
-                if(mm > vr[i].size()){
-                    mm = vr[i].size();
-                    minj = i;
-                }
-            free.remove()
-            vr[minj].push_back(i);
-            int toStart = abs(best.x - dest.x1) + abs(best.y - dest.y1);
-            int a = avto[minj-1].avail +  toStart + toDest;
-            avto[minj].update(dest.x2, dest.y2, a);
-        }
-
+    for(int i = 0 ; i < f; i++){
+        assigned_rides[i%n].push_back(rides[i].id);
     }
 
-    for(int i = 0; i< vr.size(); i++){
-        out<<vr[i].size()<<" ";
-        for(auto &e: vr[i]){
+    for(int i = 0; i< assigned_rides.size(); i++){
+        out<<assigned_rides[i].size()<<" ";
+        for(auto &e: assigned_rides[i]){
             out<<e<<" ";
         }
         out<<std::endl;
@@ -174,7 +115,7 @@ void solve(const std::string & input, const std::string & output){
 
 int main(int argc, char* argv[]) {
     if(argc < 2){
-        std::cout<<"no parameters"<<std::endl;
+        std::cout<<"No parameters"<<std::endl;
         return 1;
     }
     solve("./in/"+std::string(argv[1])+".in", "./out/"+std::string(argv[1])+".out");
